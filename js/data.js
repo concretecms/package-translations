e25b7f45132a6332906b370b00f8623e36db274b
Remo <remo.laubacher@gmail.com>
2014-03-20 21:20:26 +0100

    Refresh data

--- js/data.js
+++ js/data.js
@@ -1,1 +1,432 @@
-{"updated":1395335380,"packages":[]}
+{
+    "updated": 1395346825,
+    "packages": [
+        {
+            "handle": "constants-driller",
+            "name": "Constants Driller",
+            "sourceURL": "https://github.com/mlocati/concrete5-constants-driller",
+            "locales": {
+                "de_DE": {
+                    "translated": 33,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 33,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 33,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 33,
+                    "perc": 100
+                },
+                "el_GR": {
+                    "translated": 33,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 33,
+                    "perc": 100
+                },
+                "nl_NL": {
+                    "translated": 33,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 33,
+                    "perc": 100
+                },
+                "ro_RO": {
+                    "translated": 2,
+                    "untranslated": 31,
+                    "fuzzy": 0,
+                    "total": 33,
+                    "perc": 6
+                },
+                "zh_TW": {
+                    "translated": 33,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 33,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "cover-picture",
+            "name": "Cover Picture",
+            "sourceURL": "https://github.com/Remo/concrete5-cover-picture",
+            "locales": {
+                "de_DE": {
+                    "translated": 8,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 8,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 8,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 8,
+                    "perc": 100
+                },
+                "el_GR": {
+                    "translated": 8,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 8,
+                    "perc": 100
+                },
+                "nl_NL": {
+                    "translated": 8,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 8,
+                    "perc": 100
+                },
+                "zh_TW": {
+                    "translated": 8,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 8,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "files-cleaner",
+            "name": "Files Cleaner",
+            "sourceURL": "https://github.com/mlocati/concrete5-files-cleaner",
+            "locales": {
+                "de_DE": {
+                    "translated": 37,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 37,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 37,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 37,
+                    "perc": 100
+                },
+                "el_GR": {
+                    "translated": 37,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 37,
+                    "perc": 100
+                },
+                "nl_NL": {
+                    "translated": 37,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 37,
+                    "perc": 100
+                },
+                "zh_TW": {
+                    "translated": 37,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 37,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "irc-chat",
+            "name": "IRC Chat",
+            "sourceURL": "https://github.com/mlocati/concrete5-irc-chat",
+            "locales": {
+                "de_DE": {
+                    "translated": 39,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 39,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 39,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 39,
+                    "perc": 100
+                },
+                "el_GR": {
+                    "translated": 4,
+                    "untranslated": 35,
+                    "fuzzy": 0,
+                    "total": 39,
+                    "perc": 10
+                },
+                "nl_NL": {
+                    "translated": 39,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 39,
+                    "perc": 100
+                },
+                "zh_TW": {
+                    "translated": 39,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 39,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "list-files-from-set",
+            "name": "List Files From Set",
+            "sourceURL": "https://github.com/Mesuva/list_files_from_set",
+            "locales": {
+                "de_DE": {
+                    "translated": 34,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 34,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 34,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 34,
+                    "perc": 100
+                },
+                "nl_NL": {
+                    "translated": 34,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 34,
+                    "perc": 100
+                },
+                "zh_TW": {
+                    "translated": 34,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 34,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "localizer",
+            "name": "Localizer",
+            "sourceURL": "https://github.com/mlocati/concrete5-localizer",
+            "locales": {
+                "de_DE": {
+                    "translated": 65,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 65,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 65,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 65,
+                    "perc": 100
+                },
+                "el_GR": {
+                    "translated": 33,
+                    "untranslated": 32,
+                    "fuzzy": 0,
+                    "total": 65,
+                    "perc": 50
+                },
+                "nl_NL": {
+                    "translated": 65,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 65,
+                    "perc": 100
+                },
+                "ja_JP": {
+                    "translated": 62,
+                    "untranslated": 3,
+                    "fuzzy": 0,
+                    "total": 65,
+                    "perc": 95
+                },
+                "zh_TW": {
+                    "translated": 65,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 65,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "recaptcha",
+            "name": "reCAPTCHA",
+            "sourceURL": "https://github.com/mlocati/concrete5-recaptcha",
+            "locales": {
+                "de_DE": {
+                    "translated": 7,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 7,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 7,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 7,
+                    "perc": 100
+                },
+                "el_GR": {
+                    "translated": 7,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 7,
+                    "perc": 100
+                },
+                "nl_NL": {
+                    "translated": 7,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 7,
+                    "perc": 100
+                },
+                "zh_TW": {
+                    "translated": 7,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 7,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "remo-composer-list",
+            "name": "Remo Composer List",
+            "sourceURL": "https://github.com/Remo/concrete5-composer-list",
+            "locales": {
+                "de_DE": {
+                    "translated": 32,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 32,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 32,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 32,
+                    "perc": 100
+                },
+                "nl_NL": {
+                    "translated": 32,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 32,
+                    "perc": 100
+                },
+                "zh_TW": {
+                    "translated": 32,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 32,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "remo-page-list-attribute",
+            "name": "Remo Page List Attribute",
+            "sourceURL": "https://github.com/Remo/concrete5-pagelist-attribute",
+            "locales": {
+                "de_DE": {
+                    "translated": 11,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 11,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 11,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 11,
+                    "perc": 100
+                },
+                "nl_NL": {
+                    "translated": 11,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 11,
+                    "perc": 100
+                },
+                "zh_TW": {
+                    "translated": 11,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 11,
+                    "perc": 100
+                }
+            }
+        },
+        {
+            "handle": "zoom-image",
+            "name": "Zoom Image",
+            "sourceURL": "https://github.com/Remo/concrete5-zoom-image",
+            "locales": {
+                "de_DE": {
+                    "translated": 10,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 10,
+                    "perc": 100
+                },
+                "it_IT": {
+                    "translated": 10,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 10,
+                    "perc": 100
+                },
+                "el_GR": {
+                    "translated": 10,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 10,
+                    "perc": 100
+                },
+                "fr_FR": {
+                    "translated": 9,
+                    "untranslated": 1,
+                    "fuzzy": 0,
+                    "total": 10,
+                    "perc": 90
+                },
+                "nl_NL": {
+                    "translated": 10,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 10,
+                    "perc": 100
+                },
+                "ru_RU": {
+                    "translated": 10,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 10,
+                    "perc": 100
+                },
+                "zh_TW": {
+                    "translated": 10,
+                    "untranslated": 0,
+                    "fuzzy": 0,
+                    "total": 10,
+                    "perc": 100
+                }
+            }
+        }
+    ]
+}
