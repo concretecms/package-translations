d9cd68dbfcd56328ed4ae06ecd54b5a32c2bbe55
Remo <remo.laubacher@gmail.com>
2016-09-15 09:30:38 +0200

    Refresh data

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 {
-    "updated": 1473838224,
+    "updated": 1473924628,
     "packages": [
         {
             "handle": "addthis",
@@ -2240,11 +2240,11 @@
                     "perc": 100
                 },
                 "fa_IR": {
-                    "translated": 11,
-                    "untranslated": 27,
+                    "translated": 12,
+                    "untranslated": 26,
                     "fuzzy": 0,
                     "total": 38,
-                    "perc": 28
+                    "perc": 31
                 }
             }
         },
